2e3c3f28dd60740ec78b06b8d9224b6981d1cbd7
LuisA-Wicho <luis.romerog@uanl.edu.mx>
2024-08-23 22:22:50 -0600

    Problema 2

---
+++ Tarea_3/problema_2.py
@@ -0,0 +1,9 @@
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+
+suma = n1 + n2
+mult = n1 * n2
+resta = n1 - n2
+div = n1 / n2
+
+print ("Suma: ", suma, " Resta: ", resta, " Multiplicacion: ", mult, " Division: {:.2f}".format(div))
